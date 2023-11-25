c424785e98edf3af2cba9b0f2665db8dabae30b1
Estefania Ortega <zorleck.kongos@gmail.com>
2023-11-25 00:38:25 -0600

    Design in MagazineContainer

--- src/feature/container/magazine/MagazineContainer.jsx
+++ src/feature/container/magazine/MagazineContainer.jsx
@@ -11,12 +11,13 @@ const MagazineContainer = ({event}) => {
     <Link to="/magazine">
       <button className={`magazine-container magazine-${contextTheme}`}>
        <h1>{event.title}</h1>
+       <p></p>
        <p>{event.description}</p>
-       <p>{event.created_at}</p>
-       <p>{event.date_event}</p>
-       <p>{event.start_time}</p>
-       <p>{event.end_time}</p>
        <p>{event.location}</p>
+       <p>{event.created_at}</p>
+       <p>{event.start_time}</p>
+       <p>{event.date_event}</p>
+       <p>{event.end_time}</p>
       </button>
     </Link>
   );
