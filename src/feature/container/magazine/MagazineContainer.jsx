45b86b9a6764bab99618dbe4ad82412264051226
Rene Cruz <renecruz.0711@gmail.com>
2023-11-24 21:01:44 -0600

    Consuming tags and events endpoints

--- src/feature/container/magazine/MagazineContainer.jsx
+++ src/feature/container/magazine/MagazineContainer.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useThemeContext } from "../../../context/ThemeContext";
 import "./MagazineContainer.css";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const MagazineContainer = () => {
   const { contextTheme } = useThemeContext();
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    loadEvents();
+  }, []);
+
+  const loadEvents = async () => {
+    const result = await axios.get("http://localhost:8080/events");
+    console.log(result.data);
+    setEvents(result.data);
+  }
+
   return (
     <Link to="/magazine">
       <button className={`magazine-container magazine-${contextTheme}`}>
-        content
-        {contextTheme}
+       {
+        events.map((event, index) => {
+          <h1 key={index}>{event.title}</h1>
+        })
+       }
       </button>
     </Link>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,23 +2,24 @@ import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { useThemeContext } from "../../context/ThemeContext";
 import MagazineContainer from "../../feature/container/magazine/MagazineContainer";
-import { Axios } from "axios";
+import axios  from "axios";
 
 export default function Home() {
   const { contextTheme } = useThemeContext();
 
   //? initialize tags as empty
   const [tags, setTags] = useState([]);
-
-
   useEffect(() => {
     loadTags();
   }, []);
-
   const loadTags  = async () => {
     const result = await axios.get("http://localhost:8080/tags");
     setTags(result.data);
+    console.log(result.data)
+    console.log(tags);
   }
+
+  
 
   return (
     <div className="home-container">
@@ -37,7 +38,13 @@ export default function Home() {
         <MagazineContainer />
       </div>
       <div className="menu-filter-section">
-        <div className="tag-section">tag</div>
+        <div className="tag-section">
+          {
+            tags.map((tag, index) => (
+              <p key={index}>{tag.name}</p>
+            ))
+          }
+        </div>
         <div className="editor-section data-container">editor</div>
         <div className="reporter-section data-container">reporter</div>
       </div>
